5ca4acb59b6c226b4529ac77e93525127182b548
刘昊楠 <13495@9ji.com>
2022-11-08 16:45:34 +0800

    提交  买卖股票的最佳时机 3 但是还未解出来

--- questionOne/main.go
+++ questionOne/main.go
@@ -6,11 +6,14 @@ import (
 
 func main() {
 
-	var arr = []int{1, 2, 3, 4, 5, 6}
+	var arr = []int{1, 2, 3, 4}
 	//买卖股票的最佳时机 1
 	// fmt.Print(MaxProfitOne(arr))
 
 	//买卖股票的最佳时机 2
-	fmt.Print(MaxProfitTwo(arr))
+	// fmt.Print(MaxProfitTwo(arr))
+
+	//买卖股票的最佳时机 3
+	fmt.Print(MaxProfitThree(arr))
 
 }
